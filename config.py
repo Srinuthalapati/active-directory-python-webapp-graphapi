ecaacc8ec7aaca9dd7da0fc7eab1b14b82c6a0d1
Shanmuga S Nuthalapati <ssnuthalapati@gmail.com>
2018-05-18 11:17:43 -0700

    Changed the config details to integrate with Azure AD

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 RESOURCE = "https://graph.microsoft.com"  # Add the resource you want the access token for
-TENANT = "Your tenant"  # Enter tenant name, e.g. contoso.onmicrosoft.com
+TENANT = "TMOAAD.onmicrosoft.com"  # Enter tenant name, e.g. contoso.onmicrosoft.com
 AUTHORITY_HOST_URL = "https://login.microsoftonline.com"
-CLIENT_ID = "Your client id "  # copy the Application ID of your app from your Azure portal
-CLIENT_SECRET = "Your client secret"  # copy the value of key you generated when setting up the application
+CLIENT_ID = "502b4f50-a9fb-4522-a351-3940afe6bd1f"  # copy the Application ID of your app from your Azure portal
+CLIENT_SECRET = "fvzXNVcAjI2KYblnMVmFinA6NzE8ozWl2eHgwWtREt8="  # copy the value of key you generated when setting up the application
 
 # These settings are for the Microsoft Graph API Call
 API_VERSION = 'v1.0'
